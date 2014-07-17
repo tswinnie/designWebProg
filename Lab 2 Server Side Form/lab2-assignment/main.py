ce5ed011165fb8cece690c8d4353ed5cefbc799f
tswinnie <tyrone.swinnie@gmail.com>
2014-07-17 02:08:18 -0400

    successfully added all of the fields
    I have added all of the fields to the page

--- Lab 2 Server Side Form/lab2-assignment/main.py
+++ Lab 2 Server Side Form/lab2-assignment/main.py
@@ -19,12 +19,19 @@ class MainHandler(webapp2.RequestHandler):
 <form method ="GET" action="">
 <label>First Name</label><input type="text" name="first" />
 <label>Last Name</label><input type="text" name="last" /><br>
-<input type="checkbox" name= "car" value="Car Loan" />Car Loan<br>
+<label>Car Loan</label><input type="checkbox" name= "car" value="Car Loan" />
+
 
 <select name="small_loan">
   <option  value="$500 - $1000" >$500 - $1000</option>
     <option  value="$1000 - $2000" >$1000 - $2000</option>
         <option  value="$2000 - $3000" >$2000 - $3000</option>
+</select>
+
+<select name="loan_date">
+  <option  value="30 days - 60 days" >30 days - 60 days</option>
+    <option  value="60 days - 90 days" >60 days - 90 days</option>
+        <option  value="90 days - 120 days" >90 days - 120 days/option>
 </select>
 
 <input type="submit" value="submit" />
@@ -43,8 +50,8 @@ class MainHandler(webapp2.RequestHandler):
             last_name = self.request.GET['last']
             car_loan = self.request.GET['car']
             drop = self.request.GET['small_loan']
-            # drop_two = self.request.GET['small_loanTwo']
-            self.response.write(first_name + last_name + car_loan + drop)
+            loan = self.request.GET['loan_date']
+            self.response.write(first_name + last_name + car_loan + drop + loan)
         else:
             self.response.write(page_head + page_body + page_close)
 
