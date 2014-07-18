580c184ed0b8ce1c3c95899ed958f7c32cd553b2
tswinnie <tyrone.swinnie@gmail.com>
2014-07-17 23:33:34 -0400

    added getter
    created a getter and created a function that will hold the private
    variable

--- Encapsulated Calculator/passer-ratingcalculator/main.py
+++ Encapsulated Calculator/passer-ratingcalculator/main.py
@@ -18,7 +18,40 @@ class MainHandler(webapp2.RequestHandler):
         matt.yards = 1000
         matt.touchdowns = 100
         matt.interceptions = 3
-        
+        print matt.passer_rating
+
+                #  create instance of my QBR object
+        payton = QBR()
+        payton.passing = 400
+        payton.completion = 90
+        payton.yards = 2000
+        payton.touchdowns = 20
+        payton.interceptions = 9
+
+                #  create instance of my QBR object
+        matt = QBR()
+        matt.passing = 190
+        matt.completion = 300
+        matt.yards = 1830
+        matt.touchdowns = 50
+        matt.interceptions = 9
+
+                #  create instance of my QBR object
+        tom = QBR()
+        tom.passing = 190
+        tom.completion = 700
+        tom.yards = 1109
+        tom.touchdowns = 29
+        tom.interceptions = 20
+
+                #  create instance of my QBR object
+        drew = QBR()
+        drew.passing = 400
+        drew.completion = 290
+        drew.yards = 4000
+        drew.touchdowns = 200
+        drew.interceptions = 40
+
 
 
 
@@ -29,9 +62,11 @@ class QBR(object):  # created class that will hold the data for five players
         self.yards = 0  # this is the yards gained value
         self.touchdowns = 0  # this is the number of touchdowns value
         self.interceptions = 0  # this is the interceptions by the quarterback
-        self.__rating__ = 0  # this my private variable
+        self.__rating_ = 90  # this my private variable
 
-
+    @property
+    def passer_rating(self):
+        return self.__rating_
 
 
 
